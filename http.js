32c433c1b025cfe8b0d0e0e4ea81ed43c2e037e0
baltazarparra <baltazarparra@outlook.com>
2017-10-09 06:59:49 -0300

    Initial commit

---
+++ http.js
@@ -0,0 +1,24 @@
+const router = require('./router')
+const app = router(3412)
+
+const operadoras = [
+	{nome: 'Oi', codigo: 14, categoria: 'Celular', preco: 2},
+	{nome: 'Vivo', codigo: 15, categoria: 'Celular', preco: 1},
+	{nome: 'Tim', codigo: 41, categoria: 'Celular', preco: 3},
+]
+
+const contatos = [
+	{id: 1, nome: 'Bruno', tel: '9999-2222', data: new Date(), operadora: operadoras[0]},
+	{id: 2, nome: 'Sandria', tel: '9999-3333', data: new Date(), operadora: operadoras[1]},
+	{id: 1, nome: 'Juana', tel: '9999-9999', data: new Date(), operadora: operadoras[2]}
+]
+
+app.get('/operadoras', (req, res) => {
+	res.write(JSON.stringify(operadoras))
+	res.end()
+})
+
+app.get('/contatos', (req, res) => {
+	res.write(JSON.stringify(contatos))
+	res.end()
+})
